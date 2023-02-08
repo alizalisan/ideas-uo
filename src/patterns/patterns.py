3a42af22602bbaf7a46e73093f8c0539a33c0626
Aliza Lisan <alisan@bat.local>
2023-02-08 12:26:59 -0800

    checks for directory path

--- src/patterns/patterns.py
+++ src/patterns/patterns.py
@@ -728,7 +728,7 @@ class Patterns(Fetcher):
             directory_df = work_df[work_df['filepath'].str.contains(directory_path)]
             if(not len(directory_df)):
                 err('The given directory does not exist')
-                return 0, 0, 0, 0, 0
+                return 0, pd.DataFrame(), pd.DataFrame(), 0, pd.DataFrame()
             #*1 sums the value of locc_metric against each author on a certain file
             d = pd.DataFrame(directory_df.groupby(['filepath', 'unique_author'])[locc_metric].sum())
             d["dev_knowledge"] = 0
