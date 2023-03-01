198ff6633154dd87c0c5329ffbdbe092a7ab1923
Aliza Lisan <alisan@bat.local>
2023-02-28 23:15:42 -0800

    debug changes in mul-changes-equal

--- src/patterns/patterns.py
+++ src/patterns/patterns.py
@@ -784,7 +784,7 @@ class Patterns(Fetcher):
             #aggregating the knowledge on each file to project/directory level
             temp_df = d.drop(columns=['filepath', locc_metric])
             aggregated_df = pd.DataFrame(temp_df.groupby(['unique_author'])['dev_knowledge'].sum())
-            #aggregated_df['dev_knowledge'] = aggregated_df['dev_knowledge']/tot_files
+            aggregated_df['dev_knowledge'] = aggregated_df['dev_knowledge']/tot_files
             aggregated_df.reset_index(inplace=True)
             aggregated_df.sort_values(by=['dev_knowledge'], ascending=False, inplace=True)
 
@@ -799,7 +799,7 @@ class Patterns(Fetcher):
             
             authors_commits_df.sort_values(by=['dev_knowledge'], ascending=False, inplace=True)
             
-            display(authors_commits_df.head(5))
+            #display(authors_commits_df.head(5))
             #results = authors_commits_df
             results = aggregated_df
 
