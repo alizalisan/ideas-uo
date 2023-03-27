d571f566b5b0b3c63caf4183ba62dbd847bcc7d0
Aliza Lisan <alisan@bat.local>
2023-03-27 00:30:26 -0700

    testing

--- src/patterns/patterns.py
+++ src/patterns/patterns.py
@@ -161,7 +161,7 @@ class Patterns(Fetcher):
     excluded_subpaths = ["contrib/", "extern/", "external/"]
 
     # These are global, and generally applicable, unless a project-specific name is provided
-    default_branches = ["/main", "/master", "/develop"]
+    default_branches = ["/develop", "/main", "/master"]
 
     @staticmethod
     def is_code(path_str):
@@ -743,10 +743,10 @@ class Patterns(Fetcher):
             work_df = work_df[~work_df.branch.str.contains(branches[i])]
         
         work_df = branch_df
-        print(branches)
-        print(len(work_df))
-        display(work_df.head(5))
-        display(work_df.tail(5))
+        # print(branches)
+        # print(len(work_df))
+        # display(work_df.head(5))
+        # display(work_df.tail(5))
 
         if(not len(work_df)):
             err('The given branch(es) do(es) not exist')
